86dbba1a50b8b447b47ff81f459526120279881b
Cyanox62 <infernogaming621@gmail.com>
2019-04-26 21:12:54 -0400

    Initial commit

---
+++ EscapeItems/EscapeItems.cs
@@ -0,0 +1,28 @@
+﻿using Smod2.Attributes;
+using scp4aiur;
+
+namespace EscapeItems
+{
+	[PluginDetails(
+	author = "Cyanox",
+	name = "Escape Items",
+	description = "Allows you to keep your items when you escape.",
+	id = "cyan.escapeitems",
+	version = "1.0.0",
+	SmodMajor = 3,
+	SmodMinor = 0,
+	SmodRevision = 0
+	)]
+	public class EscapeItems : Smod2.Plugin
+	{
+		public override void OnDisable() { }
+
+		public override void OnEnable() { }
+
+		public override void Register()
+		{
+			Timing.Init(this);
+			AddEventHandlers(new EventHandler());
+		}
+	}
+}
---
+++ EscapeItems/EventHandler.cs
@@ -0,0 +1,51 @@
+﻿using Smod2.API;
+using Smod2;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System.Collections.Generic;
+using System.Linq;
+using scp4aiur;
+
+namespace EscapeItems
+{
+	class EventHandler : IEventHandlerCheckEscape, IEventHandlerSpawn
+	{
+		private Dictionary<string, List<ItemType>> pItems = new Dictionary<string, List<ItemType>>();
+
+		private List<ItemType> getPlayerItems(Player player)
+		{
+			foreach (KeyValuePair<string, List<ItemType>> entry in pItems.Where(x => x.Key == player.SteamId))
+			{
+				return entry.Value;
+			}
+			return null;
+		}
+
+		public void OnSpawn(PlayerSpawnEvent ev)
+		{
+			Timing.InTicks(() =>
+			{
+				List<ItemType> pList = getPlayerItems(ev.Player);
+				if (pList != null)
+				{
+					int c = 0;
+					while (ev.Player.GetInventory().Count < 8)
+					{
+						ev.Player.GiveItem(pList[c]);
+						c++;
+					}
+					for (int i = c; i < pList.Count; i++)
+					{
+						PluginManager.Manager.Server.Map.SpawnItem(pList[i], ev.Player.GetPosition(), Vector.Zero);
+					}
+					pItems.Remove(ev.Player.SteamId);
+				}
+			}, 8);
+		}
+
+		public void OnCheckEscape(PlayerCheckEscapeEvent ev)
+		{
+			pItems.Add(ev.Player.SteamId, ev.Player.GetInventory().Select(x => x.ItemType).ToList());
+		}
+	}
+}
